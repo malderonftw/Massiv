88a385c69822d57982435da61639ab31b46c4807
malderonftw <74318573+malderonftw@users.noreply.github.com>
2020-12-04 00:14:17 +0300

    ПП

---
+++ Assets/MoveSlowly.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSlowly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform sunrise;
+    public Transform sunset;
+    private Vector3 StartPos;
+    private float t;
+
+    // Time to move from sunrise to sunset position, in seconds.
+    private float journeyTime = 20;
+
+    // The time at which the animation started.
+    private float startTime;
+
+    void Start()
+    {
+        StartPos = sunrise.position;
+        //StartCoroutine(MoveToPosition(sunrise, sunset.position, journeyTime));
+    }
+
+    void Update()
+    {
+        t += Time.deltaTime / journeyTime;
+        transform.position = Vector3.Lerp(StartPos, sunset.position, t);
+        
+        
+    }
+    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+    {
+        var currentPos = transform.position;
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/OrderOnLine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderOnLine : MonoBehaviour
+{
+    public GameObject[] GOArray;
+    void Start()
+    {
+        var temp = GOArray[0].transform.position;
+
+        foreach (var Go in GOArray)
+        {
+            Go.gameObject.transform.position = temp;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
